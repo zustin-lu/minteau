5841e5f4fd69fa66abbeda91965b907af9b15d1a
Justin Lu <lmint.dev@gmail.com>
2021-12-10 21:25:47 +0700

    feat: init

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,161 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import { useRef, useState } from 'react';
+import Head from 'next/head';
+import Image from 'next/image';
 
 export default function Home() {
+  const pwdRef = useRef();
+  const [phase, setPhase] = useState(1);
+
+  const [showNo, setShowNo] = useState(true);
+  const [accepted, setAccepted] = useState(false);
+
+  const handleSubmit = () => {
+    const pwdValue = pwdRef.current.value;
+    if (pwdValue === 'travai') {
+      setPhase(2);
+    } else {
+      alert('Sai mật khẩu gòiiii bạn uii');
+    }
+  };
+
   return (
-    <div className={styles.container}>
+    <>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>For Minteau</title>
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+      {phase === 1 && (
+        <div
+          className="container px-12 py-4 flex flex-col items-center justify-center"
+          style={{ height: '100vh ' }}
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+          <div
+            className="rounded-full overflow-hidden relative"
+            style={{ width: 160, height: 160 }}
+          >
+            <Image
+              src="/images/minteau.jpeg"
+              layout="fill"
+              className="rounded"
+            />
+          </div>
+
+          <div className="space-y-3">
+            <div className="text-sm font-normal text-gray-500 mt-6">
+              Mật khẩu là nước uống yêu thích của em, viết liền không dấu
+            </div>
+
+            <input
+              ref={pwdRef}
+              class="border rounded w-full py-2 px-3 text-gray-700 leading-tight"
+              type="password"
+              placeholder="Mời yangho nhập mật khẩu"
+            />
+
+            <button
+              class="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full"
+              onClick={handleSubmit}
+            >
+              Dzoooooooo
+            </button>
+          </div>
+        </div>
+      )}
+
+      {phase === 2 && (
+        <div className="p-5">
+          <h1 className="text-lg font-medium mb-3">Chào Minteau,</h1>
+          <p className="text-base font-light leading-7">
+            Là lmint đây!
+            <br />
+            Anh được biết là em đang thi và anh hông biết tặng gì để chúc em đạt
+            được kết quả tốt nữa. Hôm nay rảnh rỗi, anh ngồi làm cái ứng dụng
+            nhỏ nhỏ xàm xí này mong em được vui và qua nó anh muốn gửi lời chúc
+            tới em mong em những điều tốt đẹp nhứttt
+          </p>
+          <div
+            className="relative mx-auto"
+            style={{ width: '60vw', height: '64vw' }}
+          >
+            <Image src="/images/phuclong.png" layout="fill" />
+          </div>
+
+          <p className="text-base font-light leading-7">
+            Tui để ly trà vải ở đây, đợi khi nào hết dịch và ai đó đạt được điểm
+            cao thì tới lấy nha nha! Sẵn tiện tui cũng có đôi lời xàm xí... Bạn
+            nào dễ thương muốn xem tiếp thì cứ ấn cái nút xanh xanh ở dưới nhó
+            😜
+          </p>
+
+          <button
+            class="bg-blue-600 hover:bg-blue-700 text-white py-2 px-4 rounded w-full mt-5"
+            onClick={() => setPhase(3)}
+          >
+            Hm... đọc tiếp
+          </button>
+          <p
+            className="text-blue-600 mt-3 w-full text-center"
+            onClick={() => setPhase(1)}
+          >
+            Trở về
+          </p>
+        </div>
+      )}
+
+      {phase === 3 && (
+        <div className="p-5">
+          <h1 className="text-lg font-medium mb-3">Đôi lời xàm xí,</h1>
+          <p className="text-base font-light leading-7">
+            Hm... Em đã bước qua một vài câu chuyện buồn, và vài điều không đáng
+            để nhớ. Anh ở đây mong được làm bạn với em, làm trò cùng em mong một
+            ngày nào đó cho em có được một câu chuyện tốt đẹp hơn 👊🏼👊🏼
+          </p>
+          <p className="text-base font-light leading-7">
+            Tuy anh là một IT nhạt nhẽo, nhưng mà anh cũng biết làm trò con bò
+            cho em vui đó nha. Anh còn biết học nấu ăn, biết đàn hát, biết đi
+            làm kiếm xiền mua đồ ăn vặt nựa. Nhưng anh cũng có vài khuyết điểm.
+            Nói về con người thì anh hơi nhỏ con, hông có cao to 8 múi như con
+            nhà người ta. Nhưng anh có thể bảo vệ em theo cách khác, của riêng
+            anh 🙈 Nói về học thì anh tự học hầu hết mọi thứ nên anh hông có
+            nhiều bằng cấp như bao người. Nhưng mà hiện tại công việc anh cũng
+            đã rất ổn và anh lo đc cho gia đình rồi nên anh nghĩ đó cũng hông
+            còn là vấn đề lớn nữaa. <br /> Anh nghĩ mình xứng đáng có một cơ hội
+            để theo đuổi Minteau, được khummm!
+          </p>
+
+          {!accepted ? (
+            <button
+              class="bg-green-600 hover:bg-green-700 text-white py-2 px-4 rounded w-full mt-3"
+              onClick={() => setAccepted(true)}
+            >
+              Xứng đáng có 01 cơ hội 😂
+            </button>
+          ) : (
+            <>
+              <p className="text-base font-light leading-7 mt-3">
+                Chúc em thi tốt!!
+                <br />
+                Có phúc long và có cả anh 😜
+              </p>
+            </>
+          )}
+          {showNo && (
+            <button
+              class="bg-red-600 hover:bg-red-700 text-white py-2 px-4 rounded w-full mt-3 "
+              onClick={() => setShowNo(false)}
+            >
+              Khummmmmm
+            </button>
+          )}
+
+          <p
+            className="text-blue-600 mt-3 w-full text-center"
+            onClick={() => setPhase(2)}
+          >
+            Trở về
+          </p>
+        </div>
+      )}
+    </>
+  );
 }
