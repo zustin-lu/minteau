e4ed476e65b78872c364bbc1a14b90a3d4d257e0
Justin Lu <lmint.dev@gmail.com>
2021-12-25 10:37:18 +0700

    feat: add newfeeds update announcement

--- components/pageNewsfeed/createBtn.tsx
+++ components/pageNewsfeed/createBtn.tsx
@@ -8,18 +8,21 @@ const CreateFeedBtn: FC = () => {
   const { props, actions } = useModal({ title: 'Tạo bài đăng' });
 
   return (
-    <>
+    <div className="grid grid-cols-12 gap-x-3">
+      <div className="col-span-8 text-xs text-gray-700 flex items-center">
+        Toi vừa sửa mấy cái lỗi linh tinh để tụi mình hem bị lỗi nựa ❤️
+      </div>
       <Button
         variant="info"
         onClick={actions.open}
-        className="sticky top-14 z-10 block ml-auto"
+        className="sticky top-14 z-10 block col-span-4"
       >
         Tạo bài đăng
       </Button>
       <Modal {...props}>
         <Form onSubmited={actions.hide} />
       </Modal>
-    </>
+    </div>
   );
 };
 
