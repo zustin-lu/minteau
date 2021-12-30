dd08064271e91229b6951a781284bdd4cd968218
Justin Lu <lmint.dev@gmail.com>
2021-12-30 09:35:16 +0700

    feat: add slider counter

--- components/pageNewsfeed/slider.tsx
+++ components/pageNewsfeed/slider.tsx
@@ -13,11 +13,17 @@ type Props = {
 
 const FeedSlider: FC<Props> = ({ pictures }) => {
   const [loaded, setLoaded] = useState<boolean>(false);
+  const [currentSlide, setCurrentSlide] = useState<number>(1);
+
   const [slideRef, instanceRef] = useKeenSlider({
     loop: true,
     initial: 0,
     created() {
       setLoaded(true);
+    },
+    slideChanged(slider) {
+      const idx = slider.track.absToRel(slider.track.details.abs);
+      setCurrentSlide(idx + 1);
     },
   });
 
@@ -29,6 +35,9 @@ const FeedSlider: FC<Props> = ({ pictures }) => {
             <img src={url} style={{ maxWidth: '100%' }} />
           </div>
         ))}
+      </div>
+      <div className="absolute top-2 right-2 px-2 py-1 text-white text-xs bg-gray-600 bg-opacity-30 rounded-md">
+        {currentSlide}/{instanceRef.current?.track.details.length + 1}
       </div>
       {pictures?.length > 1 && loaded && instanceRef.current && (
         <>
